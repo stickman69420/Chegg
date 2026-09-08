8bb4ffab93b08eebb3ee480e36840f37cb900983
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2026-09-08 15:53:30 +1000

    Add files via upload

--- definitions.js
+++ definitions.js
@@ -9,7 +9,7 @@ function imagesLoaded() {
 	allDone()
 }
 
-const unitnames = ["villager","zombie","creeper","pig","rabbit","pufferfish","iron_golem","frog","skeleton","blaze"]
+const unitnames = ["villager","zombie","creeper","pig","rabbit","pufferfish","iron_golem","frog","skeleton","blaze","phantom","enderman"]
 let loadedc = 0
 let loaded = false
 let units = []
