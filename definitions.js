41dfd87b222b20d9ef70f0df35103a078b91354e
stickman69420 <124035958+stickman69420@users.noreply.github.com>
2026-09-08 10:13:55 +1000

    Add files via upload

---
+++ definitions.js
@@ -0,0 +1,45 @@
+function imageLoaded() {
+	loadedc++
+	loadBar.value = loadedc/maxload
+	if (loadedc == maxload) imagesLoaded()
+}	
+function imagesLoaded() {
+	loadBarDiv.remove()
+	loaded = true
+	allDone()
+}
+
+const unitnames = ["villager","zombie","creeper","pig","rabbit","pufferfish","iron_golem","frog","skeleton","blaze"]
+let loadedc = 0
+let loaded = false
+let units = []
+let unitimgs = []
+let deck = localStorage.getItem("deck") ? JSON.parse(localStorage.getItem("deck")) : []
+let deck2 = localStorage.getItem("deck2") ? JSON.parse(localStorage.getItem("deck2")) : []
+			
+async function loadJSON(url,func) {
+	try {
+		const request = new Request(url);
+		const response = await fetch(request);
+		const done = await response.json()
+		let args = []
+		for (let i = 0; i < arguments.length; ++i) args[i] = arguments[i]
+		args.splice(0,2)
+		func(done,...args)
+		imageLoaded()
+		return(done)
+	} catch (err) {
+		alert(err)
+	}
+}
+
+function loadUnits() {
+	unitnames.forEach((e,ee) => {
+		loadJSON("./units/"+e+".json",function (e,i) {
+			units[i] = (e)
+			unitimgs[i] = new Image()
+			unitimgs[i].src = "./images/units/"+units[i].img+".png"
+			unitimgs[i].onload = imageLoaded
+		},ee)
+	})
+}
